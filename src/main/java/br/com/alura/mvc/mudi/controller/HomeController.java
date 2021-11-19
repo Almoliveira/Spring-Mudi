5abd516e9ed86015911d1acaef0c545f310d3413
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-11-19 05:55:14 -0300

    Aula 3 Finalizada

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,29 +1,27 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 10");
-		pedido.setUrlProduto("https://www.amazon.com.br/Xiaomi-Redmi-Note-Vers%C3%A3o-global/dp/B08YFLGZB4/ref=sr_1_5?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2TCUHBB916L7S&keywords=xiaomi+redmi+note+10&qid=1637274706&sprefix=xiamoi+red%2Caps%2C317&sr=8-5&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/61bD+2ZxGyS._AC_SL1500_.jpg");
-		pedido.setDescricao("Uma descrição qualquer para esse pedido");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos",pedidos);
-		
 		return "home";
 	}
+	
 }
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -3,17 +3,41 @@ package br.com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataEntrega;
+	
+	@Column(length = 1000)
 	private String urlProduto;
+	
+	@Column(length = 1000)
 	private String urlImagem;
+	
 	private String descricao;
 	
 	
 	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
